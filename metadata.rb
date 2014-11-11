2ddcda281c9cf6987653f1466b312def6a8cf02a
Jamie Winsor <jamie@vialstudios.com>
2014-11-11 11:59:19 -0800

    update license in cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ require "chef/validation/version"
 name             "validation"
 maintainer       "Jamie Winsor"
 maintainer_email "jamie@vialstudios.com"
-license          "All rights reserved"
+license          "MIT"
 description      "Installs/Configures validation"
 long_description "Installs/Configures validation"
 version           Chef::Validation::VERSION
