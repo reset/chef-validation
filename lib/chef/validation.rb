37abd050fb97822f60d28889a430698583cab2b1
Jamie Winsor <jamie@vialstudios.com>
2015-07-17 12:41:39 -0700

    fix error message when a cookbook is not found

--- lib/chef/validation.rb
+++ lib/chef/validation.rb
@@ -60,7 +60,7 @@ module Chef::Validation
       def validate_one(node, name)
         total_errors = {}
         unless cookbook = ContextExt.cookbook(node.run_context, name)
-          raise "Cookbook not found: #{cookbook}"
+          raise "Cookbook not found: #{name}"
         end
         reload_metadata(cookbook)
         unless (errors = Validator.run(node, cookbook.metadata)).empty?
